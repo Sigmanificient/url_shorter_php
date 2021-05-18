a0bb2c7d75e38953c50715e80535fdc66f04c570
Sigmanificient <edhyjox@gmail.com>
2021-05-19 01:00:08 +0200

    Very simple router + AppController shadow

---
+++ src/Controllers/AppController.php
@@ -0,0 +1,14 @@
+<?php
+
+
+class AppController
+{
+    public function read()
+    {
+
+    }
+
+    public function create() {
+
+    }
+}
--- src/public/index.php
+++ src/public/index.php
@@ -2,3 +2,10 @@
 define('ROOT', dirname(__DIR__));
 define('SITE', (($_SERVER['SERVER_PORT'] == 443) ? "https://" : "http://") . $_SERVER['SERVER_NAME']);
 
+$uri = $_GET['url'];
+$action = (strlen($uri) === 0)? "create" : "read";
+
+require_once ROOT . '/Controllers/AppController.php';
+
+$controller = new AppController();
+$controller->$action();
