33fb5c9828904690001589fdd6d5d44f0537a938
Sigmanificient <edhyjox@gmail.com>
2021-05-19 01:35:42 +0200

    User Model

--- src/Controllers/AppController.php
+++ src/Controllers/AppController.php
@@ -1,14 +1,23 @@
 <?php
 
+require_once ROOT . '/Models/UrlModel.php';
 
 class AppController
 {
-    public function read()
-    {
+    private $_model;
 
+    public function __construct()
+    {
+        $this->_model = new UrlModel();
     }
 
-    public function create() {
+    public function read()
+    {
+        echo "foo";
+    }
 
+    public function create()
+    {
+        echo "bar";
     }
 }
