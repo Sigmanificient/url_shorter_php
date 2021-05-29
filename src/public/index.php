ea9fc5c6dac328295eb983913d43f1b50bc0bdcb
Sigmanificient <edhyjox@gmail.com>
2021-05-29 23:59:15 +0200

    adding IP tracking

--- src/public/index.php
+++ src/public/index.php
@@ -2,6 +2,12 @@
 define('ROOT', dirname(__DIR__));
 define('SITE', (($_SERVER['SERVER_PORT'] == 443) ? "https://" : "http://") . $_SERVER['SERVER_NAME']);
 
+session_start();
+
+if (!isset($_SESSION['IP'])) {
+    $_SESSION['IP'] = $_SERVER['REMOTE_ADDR'];
+}
+
 $uri = $_GET['url'];
 $action = (strlen($uri) === 0)? "create" : "read";
 
